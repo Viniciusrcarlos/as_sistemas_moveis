058baf99c05e325b9236195b469508cddafc93ba
Vinicius Raupp <101226503+Viniciusrcarlos@users.noreply.github.com>
2024-11-25 20:31:45 -0300

    a

--- lib/components/custom_imput.dart
+++ lib/components/custom_imput.dart
@@ -18,6 +18,7 @@ class CustomInput extends StatelessWidget {
         decoration: InputDecoration(
           label: Text(label),
           hintText: hintText,
+          
           border: OutlineInputBorder(),
         ),
       ),
